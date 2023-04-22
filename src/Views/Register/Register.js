337b65221d154eda03e1f2f0f3947c01b2a630ba
radex171 <radex171@gmail.com>
2023-04-22 12:17:39 +0200

    add inputs into registration

--- src/Views/Register/Register.js
+++ src/Views/Register/Register.js
@@ -1,7 +1,36 @@
 import "./Register.css";
 
 function Register() {
-  return <>Register</>;
+  return (
+    <main className="register">
+      <header className="register__header">
+        <h1 className="register__title">Rejestracja</h1>
+      </header>
+      <section className="register__sectionInput">
+        <section className="register__containerInput">
+          <label className="register__labelInput">
+            Podaj nazwę użytkownika
+          </label>
+          <input
+            className="register__input"
+            type="text"
+            placeholder="nazwa uzytkownika"
+          />
+        </section>
+
+        <section className="register__containerInput">
+          <label className="register__labelInput">
+            Podaj Hasło użytkownika
+          </label>
+          <input
+            className="register__input"
+            type="text"
+            placeholder="Hasło uzytkownika"
+          />
+        </section>
+      </section>
+    </main>
+  );
 }
 
 export default Register;
